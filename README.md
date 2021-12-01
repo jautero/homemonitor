f42279046655eeccaf79e6edb01a64f20eac2da0
Juha Autero <jautero@iki.fi>
2021-12-01 19:19:47 +0200

    Changed to using client object to add subscription to topic.
    Added readme.
    Added systemd service file.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Homemonitor -- Simple Python script to monitor sensors connected to Raspberry Pi
 
+## Sensors
 
+### DHT22
+
+### 1-wire
+
+## 
--- tempmonitor.py
+++ tempmonitor.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Based on example.py in dht11
 
 import RPi.GPIO as GPIO
@@ -5,7 +6,7 @@ import dht11
 import w1thermsensor
 import time
 import datetime
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 
 # initialize GPIO
 GPIO.setwarnings(True)
@@ -15,29 +16,47 @@ GPIO.setmode(GPIO.BCM)
 indoortemp = dht11.DHT11(pin=17)
 outdoortemp = w1thermsensor.W1ThermSensor()
 auth={'username':'mqtt','password':'mqtt'}
+do_configuration=True
+
+def on_connect(client, userdata, flags, rc):
+    # (Re)Subscribe to topic on connection.
+    client.subscribe("homeassistant/sensor/restart/state")
+
+def on_message(client, userdata, msg):
+    if msg.topic == "homeassistant/sensor/restart/state":
+        do_configuration=True
+
+client=mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(auth['username'],auth['password'])
+client.connect("homeassistant.local")
+client.loop_start()
 
 config_msgs=[
     ("homeassistant/sensor/indoor/config",'{"device_class": "temperature", "name": "indoor temperature", "state_topic": "homeassistant/sensor/indoor/state", "unit_of_measurement": "°C"}',0,False),
     ("homeassistant/sensor/humidity/config",'{"device_class": "humidity", "name": "indoor humidity", "state_topic": "homeassistant/sensor/humidity/state", "unit_of_measurement": "%"}',0,False),
     ("homeassistant/sensor/outdoor/config",'{"device_class": "temperature", "name": "outdoor temperature", "state_topic": "homeassistant/sensor/outdoor/state", "unit_of_measurement": "°C"}',0,False)]
 
-publish.multiple(config_msgs, hostname="homeassistant.local", auth=auth)
 
 try:
     while True:
+        if do_configuration:
+            for msg in config_msgs:
+                client.publish(*msg)
+            do_configuration=False
+            
         result = indoortemp.read()
         outresult = outdoortemp.get_temperature()
         if result.is_valid():
-            print("Last valid input: " + str(datetime.datetime.now()))
-            print("Temperature: %-3.1f C" % result.temperature)
-            publish.single("homeassistant/sensor/indoor/state", result.temperature, hostname="homeassistant.local", auth=auth)
-            print("Humidity: %-3.1f %%" % result.humidity)
-            publish.single("homeassistant/sensor/humidity/state", result.humidity, hostname="homeassistant.local", auth=auth)
-            print("Outdoors: %-3.1f C" % outresult)
-            publish.single("homeassistant/sensor/outdoor/state", outresult, hostname="homeassistant.local", auth=auth)
+            client.publish("homeassistant/sensor/indoor/state", result.temperature)
+            client.publish("homeassistant/sensor/humidity/state", result.humidity)
+            client.publish("homeassistant/sensor/outdoor/state", outresult)
 
         time.sleep(60) # Sleep one minute
 
 except KeyboardInterrupt:
     print("Cleanup")
+    client.loop_stop()
     GPIO.cleanup()
